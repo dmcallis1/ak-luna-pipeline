745db125922a086fe9fe9d1a0719d84f3331f7c0
Duncan McAllister <dmcallis@akamai.com>
2019-06-19 13:21:50 -0400

    Added compare and copy logic

--- compareProject.py
+++ compareProject.py
@@ -1,6 +1,7 @@
 import logging
 import argparse
 import os
+import shutil
 import sys
 
 # Setup logging
@@ -22,6 +23,16 @@ pipelineSnippets = [f for f in os.listdir(args.pipeline + '/templates') if os.pa
 lunaSnippets = [f for f in os.listdir(args.snippets) if os.path.isfile(os.path.join(args.snippets, f))]
 
 if pipelineSnippets == lunaSnippets:
-    log.info('Snippets are identical.')
+    log.info('Snippets are identical. No reconciliation needed.')
+    sys.exit(0)
 else:
-    log.info('Snippets are not identical.')
+    diff = [x for x in lunaSnippets if x not in pipelineSnippets]
+    log.info('Snippets are not identical - found ' + str(len(diff)) + ' new snippets.')
+    try:
+        log.info('Copying snippets from: ' + args.snippets + ' to: ' + args.pipeline + '/templates')
+        for snippet in diff:
+                shutil.copyfile(args.snippets + '/' + snippet, args.pipeline + '/templates/' + snippet)
+        sys.exit(0)
+    except Exception as e:
+        log.error('Error copying snippet: ' + snippet)
+        sys.exit(1)
